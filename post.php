2123b5b17644a79d201d8fa0a3291e4190d1d3d9
ipipip1735 <ipipip1735@163.com>
2018-12-14 17:11:37 +0800

    Update POST/PUT

--- post.php
+++ post.php
@@ -6,6 +6,8 @@
  * Time: 06:14
  */
 
-//require "header.php";
+require "info.php";
+//打印请求头
+var_dump(getallheaders());
 
 var_dump($_POST);
--- put.php
+++ put.php
@@ -6,26 +6,22 @@
  * Time: 05:08
  */
 
-require "header.php";
+require "info.php";
+//require "info.php";
+echo "----------\n";
+
 
 $method = $_SERVER["REQUEST_METHOD"];
-
 if ($method != 'PUT') die("method is $method, not is PUT");
 
 
 //导出PUT数据
-//方法一
-$putdata = fopen("php://input", "r");
-//方法二
-// $putdata = file_get_contents('php://input', 'r');
+//方法一：一次获取全部数据
+$putdata = file_get_contents('php://input', 'r');
 
-
-//每次读取1KB
-$CHUNK = 1024;
-echo header("Content-Type")."------------";
 $prefix = "resources\\upload\\";
 //解析文件类型
-switch (header("Content-Type")) {
+switch ($_SERVER["CONTENT_TYPE"]) {
     case 'image/jpeg':
         $suffix = '.jpg';
         break;
@@ -39,12 +35,40 @@ switch (header("Content-Type")) {
         $suffix = '.txt';
 }
 
-//创建或打开临时文件
-$fp = fopen($prefix . time() . $suffix, "w");
+file_put_contents($prefix . time() . $suffix, $putdata);
 
-//每次读取1KB写入到文件
-while ($data = fread($putdata, $CHUNK)) fwrite($fp, $data);
 
-//关闭流，释放资源
-fclose($fp);
-fclose($putdata);
+
+
+
+//
+////方法二，大文件上传，每次读取1KB
+//$putdata = fopen("php://input", "r");
+//
+////每次读取1KB
+//$CHUNK = 1024;
+//$prefix = "resources\\upload\\";
+////解析文件类型
+//switch ($_SERVER["CONTENT_TYPE"]) {
+//    case 'image/jpeg':
+//        $suffix = '.jpg';
+//        break;
+//    case 'image/gif':
+//        $suffix = '.gif';
+//        break;
+//    case 'image/png':
+//        $suffix = '.png';
+//        break;
+//    default:
+//        $suffix = '.txt';
+//}
+//
+////创建或打开临时文件
+//$fp = fopen($prefix . time() . $suffix, "w");
+//
+////每次读取1KB写入到文件
+//while ($data = fread($putdata, $CHUNK)) fwrite($fp, $data);
+//
+////关闭流，释放资源
+//fclose($fp);
+//fclose($putdata);
