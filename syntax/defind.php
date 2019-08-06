ae096e818b064fe08180e234602de560cf3c696f
ipipip1735 <ipipip1735@163.com>
2019-08-06 16:57:03 +0800

    Update syntax

---
+++ syntax/defind.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/6
+ * Time: 16:46
+ */
+
+define('APP_PATH', __DIR__ . '/../application/');
+
+if(defined('APP_PATH')) echo APP_PATH;
